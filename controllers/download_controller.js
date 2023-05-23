72500b595b2b8d2714022ebe88ebb42f292d3969
@k$hay <akshaynaik5787@gmail.com>
2023-05-23 08:15:53 +0530

    added download

---
+++ controllers/download_controller.js
@@ -0,0 +1,40 @@
+const Students = require('../models/students');
+const fs = require('fs');
+const path = require('path');
+
+module.exports.download = async (req, res) => {
+    try{
+
+        const students = await Students.find({})
+            .populate('interview');
+
+        let report = "Student Name, Email, DSA Marks, FE Marks, BE Marks, Status ,Company Name, Interview Date, Interview Result";
+
+        let type1 = "";
+
+        for(let student of students) {
+            type1 = student.name + ',' + student.email + ',' + student.courseMarks[0] + ',' + student.courseMarks[1] + ',' + student.courseMarks[2] + ',' + student.status;
+
+            if(student.interview.length > 0) {
+                for(let interview of student.interview) {
+                    let type2 = "";
+
+                    type2 += "," + interview.date.toString() + ',' + interview.company_name + ',' + interview.students[0].result;
+
+                    report += "\n" + type1 + type2;
+                }
+            }
+
+            else {
+                report += '\n' + type1
+            }
+        }
+
+        fs.writeFileSync('./uploads/report.csv', report);
+
+        req.flash('success', "Successfully downloaded");
+        return res.download('./uploads/report.csv');
+    }catch(err) {
+        console.log("error while downloading ", err);
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
+const downloadController = require('../controllers/download_controller');
 
 
 console.log('router loaded');
 
 router.get('/', homeController.home);
+router.get('/downloads', downloadController.download)
 router.use('/users', require('./users'));
 router.use('/students', require('./students'));
 router.use('/interview', require('./interviews'));
