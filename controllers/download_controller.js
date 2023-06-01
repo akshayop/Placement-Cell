9f11d3e4317408be0eed01b4457e409e57716541
@k$hay <akshaynaik5787@gmail.com>
2023-06-01 20:53:19 +0530

    minor changes

--- controllers/download_controller.js
+++ controllers/download_controller.js
@@ -30,20 +30,26 @@ module.exports.download = async (req, res) => {
             }
         }
 
-        const csvFile = fs.writeFile("./uploads/report.csv", report, function(err, data) {
-            if(err) {
-                console.log(err);
-                return res.redirect('back');
-            }
-            return res.download("./uploads/report.csv", (err) => {
-                if(err) {
-                    res.send({
-                        error: err,
-                        msg: "Something went wrong"
-                    })
-                }
-            });
-        });
+        // const csvFile = fs.writeFile("./uploads/report.csv", report, function(err, data) {
+        //     if(err) {
+        //         console.log(err);
+        //         return res.redirect('back');
+        //     }
+        //     return res.download("./uploads/report.csv", (err) => {
+        //         if(err) {
+        //             res.send({
+        //                 error: err,
+        //                 msg: "Something went wrong"
+        //             })
+        //         }
+        //     });
+        // });
+
+        fs.writeFileSync("./uploads/report.csv", report);
+
+        req.flash("success", "Downloaded CSV report!");
+        return res.download("./uploads/report.csv");
+        
     }catch(err) {
         console.log('error', err);
         return res.redirect('back');
