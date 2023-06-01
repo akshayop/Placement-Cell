5915c1160dfb19fb6e0f4eaf87c8485ee39988aa
@k$hay <akshaynaik5787@gmail.com>
2023-06-01 11:21:28 +0530

    minnor change in db

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 
-const db = mongoose.connect('mongodb+srv://akshaynaik5787:Akshay123@skilltest.pdpmace.mongodb.net/placement-Cell?retryWrites=true&w=majority');
+mongoose  
+    .connect('mongodb+srv://akshaynaik5787:Akshay123@skilltest.pdpmace.mongodb.net/placement-Cell?retryWrites=true&w=majority')
+    .then(()=>{console.log("db connected")})
+    .catch((err)=>{console.log("db not connected..", err)})
 
-db.then( () => {
-    console.log("successfully connected to mongodb");
-}).catch( (err) => {
-    console.log("Error while connect db:", err);
-});
+let db = mongoose.connection;
+
+db.on('error', ()=>{console.error.bind(console,'something wrong in connection')});
+
+db.once('open',()=>{console.log("db connection successfull...")});
 
 module.exports = db;
